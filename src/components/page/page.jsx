f314d279dea3035dbc91eb0c4af53e8503a86a76
GWNR71517\joshu <joshuabliven93@gmail.com>
2022-09-15 17:23:26 -0400

    Weather app starter files

---
+++ src/components/page/page.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import Header from '../header';
+
+const Page = () => {
+    return (
+        <Fragment>
+            <Header />
+        </Fragment>
+    );
+};
+
+export default Page;
